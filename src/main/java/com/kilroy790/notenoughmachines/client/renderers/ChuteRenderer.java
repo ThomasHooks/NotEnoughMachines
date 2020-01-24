7307179f4fe4c93f4a35eeaafb0b7aacbb8f03aa
ThomasHooks <56377167+ThomasHooks@users.noreply.github.com>
2020-01-24 13:20:55 -0500

    Chute TESR now scrolls items depending on the direction it's facing.

--- src/main/java/com/kilroy790/notenoughmachines/client/renderers/ChuteRenderer.java
+++ src/main/java/com/kilroy790/notenoughmachines/client/renderers/ChuteRenderer.java
@@ -3,6 +3,7 @@ package com.kilroy790.notenoughmachines.client.renderers;
 import com.kilroy790.notenoughmachines.tiles.machines.ChuteTile;
 import com.mojang.blaze3d.platform.GlStateManager;
 
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
@@ -16,7 +17,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @SuppressWarnings("deprecation")
 @OnlyIn(Dist.CLIENT)
 public class ChuteRenderer extends TileEntityRenderer<ChuteTile> {
-	//TODO use TESR fast renderer instead
+	//TODO look into TESR fast renderer instead
+	//TODO fix item jittering 
+	//TODO change so that if the chute is blocked that it does not continue to scroll items
 	
 	
 	@Override
@@ -24,13 +27,33 @@ public class ChuteRenderer extends TileEntityRenderer<ChuteTile> {
 		
 		GlStateManager.pushMatrix();
 		
-		//Direction facing = tile.getBlockState().get(AbstractGeneratorBlock.FACING);
 		ItemStack stack = tile.getItemStack();
 		if(!stack.isEmpty()) {
-			//TODO add directions to the movement of items
-			GlStateManager.translated(x + tile.getItemStackDistance(), y + 0.25D, z + 0.5D);
 			
-			//TODO change to use baked models instead as this method is deprecated
+			switch(tile.getBlockState().get(HorizontalBlock.HORIZONTAL_FACING).getOpposite()) {
+			case SOUTH:
+				GlStateManager.translated(x + 0.5D, y + 0.25D, z + tile.getItemStackDistance());
+				break;
+				
+			case NORTH:
+				GlStateManager.translated(x + 0.5D, y + 0.25D, z + (1.0D - tile.getItemStackDistance()));
+				break;
+				
+			case EAST:
+				GlStateManager.translated(x + tile.getItemStackDistance(), y + 0.25D, z + 0.5D);
+				break;
+				
+			case WEST:
+				GlStateManager.translated(x + (1.0D - tile.getItemStackDistance()), y + 0.25D, z + 0.5D);
+				break;
+				
+			default:
+				//to north
+				GlStateManager.translated(x + 0.5D, y + 0.25D, z + (1.0D - tile.getItemStackDistance()));
+				break;
+			}
+			
+			//TODO change to use another method instead as this is deprecated
 			Minecraft.getInstance().getItemRenderer().renderItem(stack, TransformType.GROUND);
 		}
 		
